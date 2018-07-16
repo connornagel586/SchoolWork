f8408d8d1f7f122213d09fe2497188d838a11b28
connornagel586 <connornagel94@gmail.com>
2018-07-16 03:49:44 -0700

    Created functions to better separate code
    Changing the way key maker reads lines

--- Data Structures/p4/KeyMaker.java
+++ Data Structures/p4/KeyMaker.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class KeyMaker {
 
@@ -16,6 +17,7 @@ class KeyMaker {
 	String line;
 	String key;
 	GeneBankCreateBTree create;
+	int currentChar;
 
 	KeyMaker(GeneBankCreateBTree create) throws IOException {
 		this.create = create;
@@ -23,77 +25,126 @@ class KeyMaker {
 		file = create.getFile();
 		tree = new BTree(create.getDegree(), create.getFile());
 		scan = new Scanner(create.getFile());
+		currentChar = 0;
 
 	}
 
-	long getNextKey() throws Exception {
+	long getNextKey() throws Exception{
+		long finalKey = 0;
+		while(finalKey == 0 && state != States.END){
+			finalKey = genNext();
+		}
+		return 0;
+		}
+	private long genNext() throws Exception {
+		
 		switch (state) {
 
 		case START: {
-			while (state == States.START) {
-				while(scan.hasNextLine()){
-					line = scan.nextLine();
-				
-				
-
-				if (line == "") {
-					state = States.END;
-
-				} else if (line.contains("ORIGIN")) {
-					state = States.SEQUENCE;
-				}
-				}
-			}
+			start();
+			
+			break;
 		}
 		case SEQUENCE: {
-
-			while (state == States.IN_SEQUENCE) {
-
-				String ch = scan.next();
-				if (ch.contains("/")) {
-					state = States.END_SEQUENCE;
-					break;
-				} else if (ch.contains("n")) {
-
-					key = "";
-					state = States.UNKNOWN_CHARS;
-					break;
-				} else if (ch.contains("a") || ch.contains("t") || ch.contains("c") || ch.contains("g")) {
-					if (key.length() == create.getSequenceLength()) {
-						key = key.substring(1, create.getSequenceLength());
-						key += ch;
-						return encode(key);
-					} else {
-
-						key += ch;
-						if (key.length() == create.getSequenceLength()) {
-
-							return encode(key);
-						}
-					}
-				}
-
-			}
+			sequence();
+			
+			break;
 		}
 		case IN_SEQUENCE: {
-
-			while (state == States.IN_SEQUENCE) {
-
-				String ch = scan.next();
-				if (ch.contains("/")) {
-
-					state = States.END_SEQUENCE;
-				} else if (ch.contains("a") || ch.contains("t") || ch.contains("c") || ch.contains("g")) {
-					key += ch;
-					state = States.SEQUENCE;
-				}
-			}
+			buildSequence();
+			
 			break;
 
 		}
+		case UNKNOWN_CHARS: {
+			unknownChar();
+			state = States.SEQUENCE;
+			break;
+		}
 
 		case END_SEQUENCE: {
-			while(state == States.END_SEQUENCE)
+			endSequence();
+			break;
+		}
+		case END:
+			break;
+		default:
+			break;
+		}
+		//returns if key is not complete
+		return 0;
+	}
+	private void start(){
+		key = "";
+		while (state == States.START) {
+			while(scan.hasNextLine()){
+				line = scan.nextLine();
+			
+			if (line == null) {
+				state = States.END;
+				break;
+
+			} else if (line.contains("ORIGIN")) {
+				state = States.SEQUENCE;
+				break;
+			}
+			}
+		}
+	}
+	
+	private String sequence(){
+		while (state == States.SEQUENCE) {
+			
+			String ch = scan.next();
+			StringTokenizer token = new StringTokenizer(ch);
+			if(Character.isDigit(ch.charAt(0))){
+				ch = scan.next();
+			}
+
+			if (ch.charAt(currentChar) == '/') {
+				state = States.END_SEQUENCE;
+				break;
+			} else if (ch.charAt(currentChar) == 'n') {
+
+				state = States.UNKNOWN_CHARS;
+				break;
+			} else if (ch.contains("a") || ch.contains("t") || ch.contains("c") || ch.contains("g")) {
+				if (key.length() == create.getSequenceLength()) {
+					key = key.substring(1, create.getSequenceLength());
+					key += ch;
+				} else {
+
+					key += ch;
+					if (key.length() == create.getSequenceLength()) {
+
+						
+					}
+				}
+			}
+
+		}
+		return "";
+	}
+	
+	private void buildSequence(){
+		while (state == States.IN_SEQUENCE) {
+
+			String ch = scan.next();
+			if (ch.contains("/")) {
+
+				state = States.END_SEQUENCE;
+			} else if (ch.contains("a") || ch.contains("t") || ch.contains("c") || ch.contains("g")) {
+				key += ch;
+				state = States.SEQUENCE;
+				break;
+			}
+		}
+	}
+	private void unknownChar(){
+		
+	}
+	private void endSequence(){
+		while(state == States.END_SEQUENCE){
 			line = scan.nextLine();
 			if (line != null) {
 
@@ -104,24 +155,13 @@ class KeyMaker {
 					break;
 				}
 			} else {
-
 				state = States.END;
 				break;
 			}
-		}
-		
-		case END:{
-			
-		}
-		default:
-			return 0;
-			
-
-		}
-		 // for now
-		return 0;
+			}
+	
 	}
-
+	
 	private long encode(String seq) throws Exception {
 		if (seq.length() != create.getSequenceLength()) {
 			throw new Exception("String of length " + seq.length() + " was passed to btree with sequenceLenght of "
